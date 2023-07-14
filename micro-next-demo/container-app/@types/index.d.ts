955a995538d7657e0fde10fe016d0ca0dd9cad2b
Albert Min <sixiang.min@rea-group.com>
2023-07-14 10:04:48 +0800

    chore: fix types

---
+++ micro-next-demo/container-app/@types/index.d.ts
@@ -0,0 +1,11 @@
+declare module "micro-app-1/exposed" {
+  const Exposed: React.ComponentType;
+
+  export default Exposed;
+}
+
+declare module "micro-app-2/exposed" {
+  const Exposed: React.ComponentType;
+
+  export default Exposed;
+}
